3cc963b33991419d28077be5501ddf139a3e45ee
ayaka95 <172455399+ayaka95@users.noreply.github.com>
2024-10-25 15:13:19 +0000

    [Fix]layout

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,7 @@ class SearchesController < ApplicationController
     @range = params[:range]
     @word = params[:word]
     @search = params[:search]
-
+    
     if @range == "ユーザー"
       @users = User.looks(params[:search], params[:word])
     else
