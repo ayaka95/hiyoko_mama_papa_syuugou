5146e93190e5182f794a72528cdf8c4b97b346f1
ayaka95 <172455399+ayaka95@users.noreply.github.com>
2024-11-17 06:45:11 +0000

    [Add]admin/group

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -5,5 +5,11 @@ class Admin::GroupsController < ApplicationController
   def index
     @groups = Group.page(params[:page])
   end
+  
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to admin_groups_path, notice: 'グループを削除しました'
+  end
 
 end
