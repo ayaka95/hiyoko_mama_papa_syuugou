b0e72dc2ad6081bc792f190de13cc3c810b551da
EC2 Default User <ec2-user@ip-172-31-21-200.eu-north-1.compute.internal>
2024-12-12 10:41:08 +0000

    権限修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,10 +1,8 @@
 class Group < ApplicationRecord
 
   has_one_attached :image
-
   has_many :group_users, dependent: :destroy
   has_many :users, through: :group_users, source: :user
-  
   belongs_to :owner, class_name: 'User'
 
   validates :name, presence: true, uniqueness: true, length: { maximum: 30 }
