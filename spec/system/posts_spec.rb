728bc02fb3e58eaeaa429bc3ffca520e1c257f6e
EC2 Default User <ec2-user@ip-172-31-21-200.eu-north-1.compute.internal>
2024-12-25 06:52:24 +0000

    spec/system/posts_spec.rb 削除後のリダイレクト先が正しいか,自分の新しい投稿が正しく保存される

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe '投稿のテスト' do
   let!(:user) { create(:user) }
-  #let!(:post) { create(:post, user: user)}
+  let!(:post) { create(:post, user: user)}
   
   before do
     visit new_user_session_path
@@ -31,6 +31,9 @@ describe '投稿のテスト' do
         click_button '投稿'
         expect(page).to have_current_path post_path(Post.last)
       end
+      it '自分の新しい投稿が正しく保存される' do
+        expect{ post.create }.to change(Post, :count).by(1)
+      end
     end
     context '投稿が失敗した時' do
       it '投稿失敗後の遷移先が正しいか' do
@@ -41,24 +44,30 @@ describe '投稿のテスト' do
     end
   end
 
-  describe '編集のテスト：自分が投稿した投稿の場合' do
+  describe '投稿詳細のテスト：自分が投稿した投稿の場合' do
     before do
-      visit post_path(post)
+      visit posts_path
+      fill_in 'post_title', with: Faker::Lorem.characters(number:10)
+      fill_in 'post_body', with: Faker::Lorem.characters(number:50)
+      click_button '投稿'
+      visit post_path(Post.last)
     end
     context '表示の確認' do
       it '編集リンクが表示されているか' do
-        expect(page).to have_link "", href: edit_post_path(post)
+        expect(page).to have_link "編集", href: edit_post_path(Post.last)
+      end
+      it '削除リンクが表示されているか' do
+        expect(page).to have_link "削除", href: post_path(Post.last)
       end
     end
     context '削除が成功した時' do
-      it '削除されるか' do
-        expect{ post.destroy }.to change(Post, :count).by(-1)
-      end
       it '削除後のリダイレクト先が正しいか' do
         click_button '削除'
         expect(page).to have_current_path mypage_path
       end
+      it '削除されるか' do
+        expect{ post.destroy }.to change(Post, :count).by(-1)
+      end
     end
   end
-
 end
